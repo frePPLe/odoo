f2063751ab23fa3bc0748e1dceebf019c955472c
hichamlahlou <hlahlou@frepple.com>
2020-09-21 21:52:41 +0200

    replace uninitilized variable openerp with odoo

--- frepple/static/src/js/frepple.js
+++ frepple/static/src/js/frepple.js
@@ -1,4 +1,4 @@
-openerp.frepple = function(instance, local) {
+odoo.frepple = function(instance, local) {
   var _t = instance.web._t,
       _lt = instance.web._lt;
   var QWeb = instance.web.qweb;
