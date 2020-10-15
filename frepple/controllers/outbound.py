4e5026897a24be7d57033cec807c20ca102556e8
hichamlahlou <hlahlou@frepple.com>
2020-10-15 12:00:16 +0200

    Make sure connectors do not pull the cancelled POs

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -1040,7 +1040,11 @@ class exporter(object):
         recs = m.search(
             [
                 "|",
-                ("order_id.state", "not in", ("draft", "sent", "bid", "confirmed")),
+                (
+                    "order_id.state",
+                    "not in",
+                    ("draft", "sent", "bid", "confirmed", "cancel"),
+                ),
                 ("order_id.state", "=", False),
             ]
         )
@@ -1052,6 +1056,7 @@ class exporter(object):
             "qty_received",
             "product_uom",
             "order_id",
+            "state",
         ]
         po_line = [i for i in recs.read(fields)]
 
@@ -1067,7 +1072,7 @@ class exporter(object):
         yield "<!-- open purchase orders -->\n"
         yield "<operationplans>\n"
         for i in po_line:
-            if not i["product_id"]:
+            if not i["product_id"] or i["state"] == "cancel":
                 continue
             item = self.product_product.get(i["product_id"][0], None)
             j = po[i["order_id"][0]]
