64574b2269838e0a30e41febc3031ae69b9f8ce1
Hicham Lahlou <hlahlou@frepple.com>
2023-10-12 16:05:00 +0200

    revert custom quoteattr, this will be handled in the frepple side

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -26,7 +26,7 @@ import json
 import logging
 import pytz
 import xmlrpc.client
-from xml.sax.saxutils import quoteattr as quoteattr_generic
+from xml.sax.saxutils import quoteattr
 from datetime import datetime, timedelta
 from pytz import timezone
 import ssl
@@ -38,14 +38,6 @@ except ImportError:
     pass
 
 logger = logging.getLogger(__name__)
-
-
-def quoteattr(str):
-    return quoteattr_generic(
-        str.encode(encoding="UTF-8", errors="ignore").decode(
-            encoding="UTF-8", errors="ignore"
-        )
-    )
 
 
 class Odoo_generator:
